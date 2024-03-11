11252011674e9e173cbdb4e9cd2f2bd47174f81a
tp1415926535 <iamhermit@163.com>
2024-03-11 11:00:47 +0800

    add wpf test demo

---
+++ WpfApp.Core.Demo/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using DIAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp.Core.Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DIInject]
+        MainWindowViewModel viewModel { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ServiceSetter.InjectServices(this);
+            this.DataContext = viewModel;
+        }
+    }
+}
---
+++ WpfApp.Core.Demo/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using DIAttribute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp.Core.Demo
+{
+    [DIRegister]
+    class MainWindowViewModel
+    {
+        public string Text { get; set; } = "Hello World!";
+    }
+}
